dd8de6edafde0f394e4af7051d73c58f689dbd75
unknown <Kamilakhtartanoli@gmail.com>
2025-09-16 01:01:37 +0500

    1:01 AM

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const { database } = require('./database/db')
-const env = require('dotenv').config()
-const cors = require('cors')
-const { router } = require('./Routes/Route')
+const serverless = require("serverless-http");
+const express = require("express");
+const { database } = require("./database/db");
+require("dotenv").config();
+const cors = require("cors");
+const { router } = require("./Routes/Route");
 
-const app = express()
-const port = process.env.port || 8001
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-app.use('/api',router)
+app.use(cors());
+app.use("/api", router);
 
-database()
-app.listen(port,()=>{
-    console.log(`server started on port : ${port}`)
-})
+database();
+
+module.exports = app;
+module.exports.handler = serverless(app);
