4b0dd177e6a693d20f52b9a7c362cf5e708e367f
unknown <Kamilakhtartanoli@gmail.com>
2025-09-16 09:24:04 +0500

    9:23 AM 16-9-2025 tue

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const { database } = require('./database/db')
-const env = require('dotenv').config()
-const cors = require('cors')
-const { router } = require('./Routes/Route')
+const express = require('express');
+const { database } = require('./database/db');
+const env = require('dotenv').config();
+const cors = require('cors');
+const { router } = require('./Routes/Route');
 
-const app = express()
-const port = process.env.port || 8001
-app.use(express.json())
+const app = express();
+
+const port = process.env.PORT || 8001;
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
-app.use('/api',router)
+app.use(cors({ origin: "*" }));
 
-database()
-app.listen(port,()=>{
-    console.log(`server started on port : ${port}`)
-})
+app.use('/api', router);
+
+database();
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`✅ Server running at http://localhost:${port}`);
+});
