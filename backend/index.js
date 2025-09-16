e243040afc075244335698f59bc5af6d908710c8
unknown <Kamilakhtartanoli@gmail.com>
2025-09-16 09:31:30 +0500

    9:31 AM Tue 16-9-2025

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,17 @@
-const express = require('express');
-const { database } = require('./database/db');
-const env = require('dotenv').config();
-const cors = require('cors');
-const { router } = require('./Routes/Route');
+const express = require('express')
+const { database } = require('./database/db')
+const env = require('dotenv').config()
+const cors = require('cors')
+const { router } = require('./Routes/Route')
 
-const app = express();
-
-const port = process.env.PORT || 8001;
-
-app.use(express.json());
+const app = express()
+const port = process.env.port || 8001
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
+app.use('/api',router)
 
-app.use('/api', router);
-
-database();
-
-app.listen(port, "0.0.0.0", () => {
-  console.log(`✅ Server running at http://localhost:${port}`);
-});
+database()
+app.listen(port,()=>{
+    console.log(`server started on port : ${port}`)
+})
